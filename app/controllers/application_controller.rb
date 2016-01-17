52547c9c50817e3c8170c533e0247b06c9ceef4e
matthewbillienyc <matthewbillienyc@gmail.com>
2016-01-17 15:41:43 -0500

    added login required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :require_login, except: [:new, :create]
   include SessionsHelper
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this."
+      redirect_to root_path
+    end
+  end
 end
