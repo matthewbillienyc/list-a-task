c708b96d60007e787b23d50a893b62ef582233dc
matthewbillienyc <matthewbillienyc@gmail.com>
2016-01-29 20:25:42 -0500

    heroku stage

--- db/migrate/20160130010201_add_lists_and_tasks_total_to_users.rb
+++ db/migrate/20160130010201_add_lists_and_tasks_total_to_users.rb
@@ -2,6 +2,6 @@ class AddListsAndTasksTotalToUsers < ActiveRecord::Migration
   def change
     add_column :users, :lists_total, :integer, default: 0
     add_column :users, :tasks_total, :integer, default: 0
-    add_column :users, :admin, :boolean, default: 0
+    add_column :users, :admin, :boolean, default: false
   end
 end
