d3ad0281954feacb17b0dd5d37b0f59a8556d9ad
matthewbillienyc <matthewbillienyc@gmail.com>
2016-02-02 13:31:17 -0500

    moved all js into doc ready to fix bug

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,6 +2,112 @@
 
 $(function(){
 
+  function Ajax() { }
+  Ajax.prototype = {
+    searchListener: function(el, callback){
+      el.on("submit", function(e){
+        e.preventDefault();
+        var searchField = $("[name='keyword']");
+        var listsCheckbox = $("[name='include[list]']");
+        var tasksCheckbox = $("[name='include[task]']");
+        if ((searchField.val() == "") || ((!listsCheckbox.is(':checked')) && (!tasksCheckbox.is(':checked')))){
+          $("#flash").html("<div class='alert alert-danger'>Enter search term with at least one box checked.");
+        } else {
+          var url = this.getAttribute('action');
+          var method = this.getAttribute('method');
+          var $form = $(this);
+          $.ajax({
+            url: url,
+            method: method,
+            data: $form.serialize(),
+            success: callback
+          })
+        }
+      })
+    },
+    priorityFilterListener: function(parent, el){
+      parent.on("change", el, function(e){
+        var priority = $("select#task-filter").val();
+        var deleteCheckbox = $(".task[type='checkbox']");
+        if ((deleteCheckbox.is(':checked'))){
+          if (priority != "all"){
+            $(".task-table tr:contains("+priority+")").show();
+            $(".task-table tr:not(:contains("+priority+"))").hide();
+          } else {
+            $(".task-table tr:contains("+priority+")").show();
+          }
+        } else {
+          if (priority != "all"){
+            $(".task-table tr:contains("+priority+"):not(:contains('☓'))").show();
+            $(".task-table tr:not(:contains("+priority+")):not(:contains('☓'))").hide();
+          } else {
+            $(".task-table tr:contains("+priority+"):not(:contains('☓'))").show();
+          }
+        }
+      })
+    },
+    usernameFilterListener: function(parent, el){
+      parent.on("change", el, function(e){
+        var username = $("select#list-filter").val()
+        var deleteCheckbox = $(".list[type='checkbox']");
+        if ((deleteCheckbox.is(':checked'))){
+          if (username != "all"){
+            $(".list-table tr").show();
+            $(".list-table tr:not(:contains("+username+"))").hide();
+          } else {
+            $(".task-table tr").show();
+          }
+        } else {
+          if (username != "all"){
+            $(".list-table tr:not(:contains('☓'))").show();
+            $(".list-table tr:not(:contains("+username+")):not(:contains('☓'))").hide();
+          } else {
+            $(".task-table tr:not(:contains('☓'))").show();
+          }
+        }
+      })
+    },
+    deletedFilterListener: function(parent, el){
+      parent.on("change", el, function(e){
+        var resultsType = this.className.split(" ")[1];
+        var selectFilter = $("#"+resultsType+"-filter").val();
+        if (selectFilter != "all"){
+          if ($(this).is(":checked")){
+            $("."+resultsType+"-table tr:contains('☓'):contains("+selectFilter+")").show();
+          } else {
+            $("."+resultsType+"-table tr:contains('☓'):contains("+selectFilter+")").hide();
+          }
+        } else {
+          if ($(this).is(":checked")){
+            $("."+resultsType+"-table tr:contains('☓')").show();
+          } else {
+            $("."+resultsType+"-table tr:contains('☓')").hide();
+          }
+        }
+      })
+    }
+  }
+
+  function Callbacks() { }
+  Callbacks.prototype = {
+    results: function(dataBack){
+      $(".list-results").html("");
+      $(".task-results").html("");
+      $(".list-results").html(dataBack.list_results);
+      $(".task-results").html(dataBack.task_results);
+      $(".deleted[type='checkbox']").prop('checked', false);
+      $("tr:contains('☓')").hide();
+    }
+  }
+
+  function OptionChecker(){ }
+  OptionChecker.prototype.check = function(){
+    $(".search-option").prop('checked', true);
+  }
+
+  var ajax = new Ajax();
+  var callbacks = new Callbacks();
+  var optionChecker = new OptionChecker();
   var searchForm = $(".search-form");
   var selectTaskParent = $(".task-results");
   var selectListParent = $(".list-results");
@@ -16,110 +122,3 @@ $(function(){
   ajax.deletedFilterListener(deleteParent, deleteEl);
 
 });
-
-function Ajax() { }
-Ajax.prototype = {
-  searchListener: function(el, callback){
-    el.on("submit", function(e){
-      e.preventDefault();
-      var searchField = $("[name='keyword']");
-      var listsCheckbox = $("[name='include[list]']");
-      var tasksCheckbox = $("[name='include[task]']");
-      if ((searchField.val() == "") || ((!listsCheckbox.is(':checked')) && (!tasksCheckbox.is(':checked')))){
-        $("#flash").html("<div class='alert alert-danger'>Enter search term with at least one box checked.");
-      } else {
-        var url = this.getAttribute('action');
-        var method = this.getAttribute('method');
-        var $form = $(this);
-        $.ajax({
-          url: url,
-          method: method,
-          data: $form.serialize(),
-          success: callback
-        })
-      }
-    })
-  },
-  priorityFilterListener: function(parent, el){
-    parent.on("change", el, function(e){
-      var priority = $("select#task-filter").val();
-      var deleteCheckbox = $(".task[type='checkbox']");
-      if ((deleteCheckbox.is(':checked'))){
-        if (priority != "all"){
-          $(".task-table tr:contains("+priority+")").show();
-          $(".task-table tr:not(:contains("+priority+"))").hide();
-        } else {
-          $(".task-table tr:contains("+priority+")").show();
-        }
-      } else {
-        if (priority != "all"){
-          $(".task-table tr:contains("+priority+"):not(:contains('☓'))").show();
-          $(".task-table tr:not(:contains("+priority+")):not(:contains('☓'))").hide();
-        } else {
-          $(".task-table tr:contains("+priority+"):not(:contains('☓'))").show();
-        }
-      }
-    })
-  },
-  usernameFilterListener: function(parent, el){
-    parent.on("change", el, function(e){
-      var username = $("select#list-filter").val()
-      var deleteCheckbox = $(".list[type='checkbox']");
-      if ((deleteCheckbox.is(':checked'))){
-        if (username != "all"){
-          $(".list-table tr").show();
-          $(".list-table tr:not(:contains("+username+"))").hide();
-        } else {
-          $(".task-table tr").show();
-        }
-      } else {
-        if (username != "all"){
-          $(".list-table tr:not(:contains('☓'))").show();
-          $(".list-table tr:not(:contains("+username+")):not(:contains('☓'))").hide();
-        } else {
-          $(".task-table tr:not(:contains('☓'))").show();
-        }
-      }
-    })
-  },
-  deletedFilterListener: function(parent, el){
-    parent.on("change", el, function(e){
-      var resultsType = this.className.split(" ")[1];
-      var selectFilter = $("#"+resultsType+"-filter").val();
-      if (selectFilter != "all"){
-        if ($(this).is(":checked")){
-          $("."+resultsType+"-table tr:contains('☓'):contains("+selectFilter+")").show();
-        } else {
-          $("."+resultsType+"-table tr:contains('☓'):contains("+selectFilter+")").hide();
-        }
-      } else {
-        if ($(this).is(":checked")){
-          $("."+resultsType+"-table tr:contains('☓')").show();
-        } else {
-          $("."+resultsType+"-table tr:contains('☓')").hide();
-        }
-      }
-    })
-  }
-}
-
-function Callbacks() { }
-Callbacks.prototype = {
-  results: function(dataBack){
-    $(".list-results").html("");
-    $(".task-results").html("");
-    $(".list-results").html(dataBack.list_results);
-    $(".task-results").html(dataBack.task_results);
-    $(".deleted[type='checkbox']").prop('checked', false);
-    $("tr:contains('☓')").hide();
-  }
-}
-
-function OptionChecker(){ }
-OptionChecker.prototype.check = function(){
-  $(".search-option").prop('checked', true);
-}
-
-var ajax = new Ajax();
-var callbacks = new Callbacks();
-var optionChecker = new OptionChecker();
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,6 +2,153 @@
 
 $(function(){
 
+  function ListChecker(){ }
+  ListChecker.prototype = {
+    check: function(){
+      var lists = $(".list");
+      if (lists.length == 0){
+        $(".add-task").hide();
+      } else {
+        $(".add-task").show();
+      }
+    }
+  }
+
+  function Ajax() { }
+  Ajax.prototype = {
+    addListener: function(el, callback){
+      el.on("submit", function(e){
+        e.preventDefault();
+        var url = this.getAttribute('action');
+        var method = this.getAttribute('method');
+        var $form = $(this);
+        $.ajax({
+          url: url,
+          method: method,
+          data: $form.serialize(),
+          success: callback
+        })
+      })
+    },
+    addDelegate: function(parent, el, callback){
+      parent.on("submit", el, function(e){
+        e.preventDefault();
+        var url = this.getAttribute('action');
+        var method = this.getAttribute('method');
+        var $form = $(this);
+        $.ajax({
+          url: url,
+          method: method,
+          data: $form.serialize(),
+          success: callback
+        })
+      })
+    },
+    delegateDblclick: function(parent, el, callback){
+      parent.on("dblclick", el, function(e){
+        var className = this.className;
+        var taskId = this.getAttribute('data-id');
+        var url = '/tasks/'+taskId+'/'+className;
+        $.ajax({
+          url: url,
+          method: 'get',
+          success: callback
+        })
+      })
+    },
+    updateTaskListener: function(parent, el, callback){
+      parent.on("blur", el, function(e){
+        var $form = $(this).parent();
+        $.ajax({
+          url: $form.attr('action'),
+          method: 'patch',
+          data: $form.serialize(),
+          success: callback
+        })
+      })
+    }
+  }
+
+  function StarCallbacks() { }
+  StarCallbacks.prototype = {
+    star: function(dataBack){
+      var stype = dataBack.star.starable_type.toLowerCase();
+      $("#"+stype+"-"+dataBack.star.starable_id+" .star-span").html(dataBack.unstar_partial);
+    },
+    unstar: function(dataBack){
+      var stype = dataBack.starable_type.toLowerCase();
+      $("#"+stype+"-"+dataBack.starable_id+" .star-span").html(dataBack.star_partial);
+    }
+  }
+
+  function DeleteCallbacks() { }
+  DeleteCallbacks.prototype = {
+    deleteList: function(dataBack){
+      $("#whole-list-"+dataBack.list_id).remove();
+      $("option[value='"+dataBack.list_id+"']").remove();
+      new ListChecker().check();
+    },
+    deleteTask: function(dataBack){
+      $("#task-"+dataBack.task_id).remove();
+    }
+  }
+
+  function EditCallbacks() { }
+  EditCallbacks.prototype = {
+    priorityEdit: function(dataBack){
+      var prioritySelect = $(".priority[data-id="+dataBack.task.id+"]");
+      prioritySelect.html(dataBack.priority_partial);
+    },
+    descriptionEdit: function(dataBack){
+      var description = $(".description[data-id="+dataBack.task.id+"]");
+      description.html(dataBack.description_partial);
+    },
+    priorityUpdate: function(dataBack){
+      var priority = $(".priority[data-id="+dataBack.task.id+"]");
+      priority.html(dataBack.task.priority);
+    },
+    descriptionUpdate: function(dataBack){
+      var description = $(".description[data-id="+dataBack.task.id+"]");
+      description.html(dataBack.task.description);
+    }
+  }
+
+  function ListAppender() { }
+  ListAppender.prototype = {
+    addListeners: function(dataBack){
+      if (dataBack.list_partial) {
+        $(".lists-div").append(dataBack.list_partial);
+        $("#task_list_id").append(dataBack.options_partial);
+        $("#add-list input[type='text']").val("");
+        $("#flash").html("");
+        new ListChecker().check();
+      } else {
+        $("#flash").html(dataBack.flash_partial);
+      }
+    }
+  }
+
+  function TaskAppender() { }
+  TaskAppender.prototype = {
+    addListeners: function(dataBack){
+      if (dataBack.task){
+        $("#whole-list-"+dataBack.task.list_id+" ul").append(dataBack.task_partial);
+        $(".add-task textarea").val("");
+        $("#flash").html("")
+      } else {
+        $("#flash").html(dataBack.flash_partial);
+      }
+    }
+  }
+
+  var listChecker = new ListChecker();
+  var taskAppender = new TaskAppender();
+  var listAppender = new ListAppender();
+  var ajax = new Ajax();
+  var starCallbacks = new StarCallbacks();
+  var deleteCallbacks = new DeleteCallbacks();
+  var editCallbacks = new EditCallbacks();
+
   var listsDiv = $(".lists-div");
   var deleteListEl = ".delete-list";
   var deleteTaskEl = ".delete-task";
@@ -27,151 +174,5 @@ $(function(){
   ajax.updateTaskListener(listsDiv, prioritySelect, editCallbacks.priorityUpdate);
   ajax.updateTaskListener(listsDiv, descriptionField, editCallbacks.descriptionUpdate);
 
+
 });
-
-function ListChecker(){ }
-ListChecker.prototype = {
-  check: function(){
-    var lists = $(".list");
-    if (lists.length == 0){
-      $(".add-task").hide();
-    } else {
-      $(".add-task").show();
-    }
-  }
-}
-
-function Ajax() { }
-Ajax.prototype = {
-  addListener: function(el, callback){
-    el.on("submit", function(e){
-      e.preventDefault();
-      var url = this.getAttribute('action');
-      var method = this.getAttribute('method');
-      var $form = $(this);
-      $.ajax({
-        url: url,
-        method: method,
-        data: $form.serialize(),
-        success: callback
-      })
-    })
-  },
-  addDelegate: function(parent, el, callback){
-    parent.on("submit", el, function(e){
-      e.preventDefault();
-      var url = this.getAttribute('action');
-      var method = this.getAttribute('method');
-      var $form = $(this);
-      $.ajax({
-        url: url,
-        method: method,
-        data: $form.serialize(),
-        success: callback
-      })
-    })
-  },
-  delegateDblclick: function(parent, el, callback){
-    parent.on("dblclick", el, function(e){
-      var className = this.className;
-      var taskId = this.getAttribute('data-id');
-      var url = '/tasks/'+taskId+'/'+className;
-      $.ajax({
-        url: url,
-        method: 'get',
-        success: callback
-      })
-    })
-  },
-  updateTaskListener: function(parent, el, callback){
-    parent.on("blur", el, function(e){
-      var $form = $(this).parent();
-      $.ajax({
-        url: $form.attr('action'),
-        method: 'patch',
-        data: $form.serialize(),
-        success: callback
-      })
-    })
-  }
-}
-
-function StarCallbacks() { }
-StarCallbacks.prototype = {
-  star: function(dataBack){
-    var stype = dataBack.star.starable_type.toLowerCase();
-    $("#"+stype+"-"+dataBack.star.starable_id+" .star-span").html(dataBack.unstar_partial);
-  },
-  unstar: function(dataBack){
-    var stype = dataBack.starable_type.toLowerCase();
-    $("#"+stype+"-"+dataBack.starable_id+" .star-span").html(dataBack.star_partial);
-  }
-}
-
-function DeleteCallbacks() { }
-DeleteCallbacks.prototype = {
-  deleteList: function(dataBack){
-    $("#whole-list-"+dataBack.list_id).remove();
-    $("option[value='"+dataBack.list_id+"']").remove();
-    new ListChecker().check();
-  },
-  deleteTask: function(dataBack){
-    $("#task-"+dataBack.task_id).remove();
-  }
-}
-
-function EditCallbacks() { }
-EditCallbacks.prototype = {
-  priorityEdit: function(dataBack){
-    var prioritySelect = $(".priority[data-id="+dataBack.task.id+"]");
-    prioritySelect.html(dataBack.priority_partial);
-  },
-  descriptionEdit: function(dataBack){
-    var description = $(".description[data-id="+dataBack.task.id+"]");
-    description.html(dataBack.description_partial);
-  },
-  priorityUpdate: function(dataBack){
-    var priority = $(".priority[data-id="+dataBack.task.id+"]");
-    priority.html(dataBack.task.priority);
-  },
-  descriptionUpdate: function(dataBack){
-    var description = $(".description[data-id="+dataBack.task.id+"]");
-    description.html(dataBack.task.description);
-  }
-}
-
-function ListAppender() { }
-ListAppender.prototype = {
-  addListeners: function(dataBack){
-    if (dataBack.list_partial) {
-      $(".lists-div").append(dataBack.list_partial);
-      $("#task_list_id").append(dataBack.options_partial);
-      $("#add-list input[type='text']").val("");
-      $("#flash").html("");
-      new ListChecker().check();
-    } else {
-      $("#flash").html(dataBack.flash_partial);
-    }
-  }
-}
-
-function TaskAppender() { }
-TaskAppender.prototype = {
-  addListeners: function(dataBack){
-    if (dataBack.task){
-      $("#whole-list-"+dataBack.task.list_id+" ul").append(dataBack.task_partial);
-      $(".add-task textarea").val("");
-      $("#flash").html("")
-    } else {
-      $("#flash").html(dataBack.flash_partial);
-    }
-  }
-}
-
-var listChecker = new ListChecker();
-var taskAppender = new TaskAppender();
-var listAppender = new ListAppender();
-var ajax = new Ajax();
-var starCallbacks = new StarCallbacks();
-var deleteCallbacks = new DeleteCallbacks();
-var editCallbacks = new EditCallbacks();
